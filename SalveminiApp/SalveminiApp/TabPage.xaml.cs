0670656f1e93afbb17c0459f105e3f6aa23b3b40
Marco Coppola <murkrow@MacBook-Pro-di-Marco.local>
2019-07-29 16:00:54 -0400

    Local App

---
+++ SalveminiApp/SalveminiApp/TabPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using SalveminiApp.Helpers;
+using Xamarin.Forms;
+
+namespace SalveminiApp
+{
+    public partial class TabPage : TabbedPage
+    {
+        //Home
+        public static Xamarin.Forms.NavigationPage Home = new Xamarin.Forms.NavigationPage(new Home())
+        {
+            BarBackgroundColor = Styles.PrimaryGym,
+            BarTextColor = Color.White,
+            Title = "Home"
+        };
+        public static Xamarin.Forms.NavigationPage ArgoMenu = new Xamarin.Forms.NavigationPage(new TabPages.ArgoMenu())
+        {
+            BarBackgroundColor = Styles.PrimaryColor,
+            BarTextColor = Color.White,
+            Title = "Registro"
+        };
+
+        public TabPage()
+        {
+
+            InitializeComponent();
+            //Configure pages
+#if __IOS__
+            if (UIDevice.CurrentDevice.CheckSystemVersion(11, 0))
+            {
+                Palestra.On<Xamarin.Forms.PlatformConfiguration.iOS>().SetPrefersLargeTitles(true);
+                Home.On<Xamarin.Forms.PlatformConfiguration.iOS>().SetPrefersLargeTitles(true);
+                BarBackgroundColor = Color.White;
+            }
+#endif
+
+            //Add Children
+            Children.Add(Home);
+            Children.Add(ArgoMenu);
+
+            //Initial Page
+            SelectedItem = Children[1];
+        }
+    }
+}
---
+++ SalveminiApp/SalveminiApp/TabPages/ArgoMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SalveminiApp.TabPages
+{
+    public partial class ArgoMenu : ContentPage
+    {
+        public ArgoMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
