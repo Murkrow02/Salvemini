d6f0a88e998d8c0ff39841009f2fb6ca9802ac48
xV4L3x <valeriopio02@gmail.com>
2019-08-22 16:17:59 +0200

    some assenze and argo stuff

---
+++ SalveminiApp/SalveminiApp/Helpers/Popups/GiustificaAssenza.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SalveminiApp.Helpers.Popups
+{
+    public partial class GiustificaAssenza : ContentPage
+    {
+        public GiustificaAssenza()
+        {
+            InitializeComponent();
+        }
+    }
+}
