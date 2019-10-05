8b467fbfa437b52911e3d5fe68e90befdd548e17
Marco Coppola <murkrow@MBP-di-Marco.home-life.hub>
2019-10-05 14:16:23 +0200

    xd change

---
+++ SalveminiApp/SalveminiApp/FirstAccess/OrariTrasporti.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SalveminiApp.FirstAccess
+{
+    public partial class OrariTrasporti : ContentPage
+    {
+        public OrariTrasporti()
+        {
+            InitializeComponent();
+        }
+    }
+}
