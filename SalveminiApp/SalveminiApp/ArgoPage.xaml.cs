5477447dbeaadf8298057a6a1bfad62a58d49c2f
xV4L3x <valeriopio02@gmail.com>
2019-10-22 17:14:55 +0200

    Argo started

--- SalveminiApp/SalveminiApp/ArgoPage.xaml.cs
+++ SalveminiApp/SalveminiApp/ArgoPage.xaml.cs
@@ -11,45 +11,64 @@ namespace SalveminiApp
         {
             InitializeComponent();
 
-            //Set Sizes
-            assenzeIcon.WidthRequest = App.ScreenWidth / 3.8;
-            compitiIcon.WidthRequest = App.ScreenWidth / 3.8;
-            promemoriaIcon.WidthRequest = App.ScreenWidth / 3.8;
-            votiIcon.WidthRequest = App.ScreenWidth / 3.8;
-            argoIcon.WidthRequest = App.ScreenWidth / 3.8;
-            votiScruIcon.WidthRequest = App.ScreenWidth / 3.8;
+            //Hide Navigation Bar
+            NavigationPage.SetHasNavigationBar(this, false);
 
-
+            //Set Right Padding on Notch devices
+#if __IOS__
+            if (iOS.AppDelegate.HasNotch)
+            {
+                mainLayout.Padding = new Thickness(0, 40);
+            }
+#endif
         }
 
-        void Assenze_Tapped(object sender, System.EventArgs e)
+        protected override void OnAppearing()
         {
-            Navigation.PushModalAsync(new ArgoPages.Assenze());
+            base.OnAppearing();
+            widgetCollection.ItemsSource = new List<ArgoWidget> { new ArgoWidget { Title = "Assenze", Icon = "Assenze.svg", StartColor = "FF7272", EndColor = "FACA6F", Push = new ArgoPages.Assenze() }, new ArgoWidget { Title = "Voti", Icon = "Voti.svg", StartColor = "A940F5", EndColor = "6E9BFF", Push = new ArgoPages.Voti() }, new ArgoWidget { Title = "Promemoria", Icon = "Promemoria.svg", StartColor = "F86095", EndColor = "FF6073", Push = new ArgoPages.Promemoria() }, new ArgoWidget { Title = "Scrutinio", Icon = "VotiScru.svg", StartColor = "A940F5", EndColor = "6E9BFF", Push = new ArgoPages.VotiScrutinio() } };
+            
         }
 
-        void Promemoria_Tapped(object sender, System.EventArgs e)
+        void Widget_Tapped(object sender, EventArgs e)
         {
-            Navigation.PushModalAsync(new ArgoPages.Promemoria());
+            //Get push page from model
+            var widget = sender as ArgoWidget;
+
+            //Push to selected page
+            if (widget.Push != null)
+                Navigation.PushModalAsync(widget.Push);
         }
 
-        void Voti_Tapped(object sender, System.EventArgs e)
-        {
-            Navigation.PushModalAsync(new ArgoPages.Voti());
-        }
 
-        void VotiScru_Tapped(object sender, System.EventArgs e)
-        {
-            Navigation.PushModalAsync(new ArgoPages.VotiScrutinio());
-        }
+        //void Assenze_Tapped(object sender, System.EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new ArgoPages.Assenze());
+        //}
 
-        void Compiti_Tapped(object sender, System.EventArgs e)
-        {
-            Navigation.PushModalAsync(new ArgoPages.CompitiArgomenti("compiti"));
-        }
+        //void Promemoria_Tapped(object sender, System.EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new ArgoPages.Promemoria());
+        //}
 
-        void Argomenti_Tapped(object sender, System.EventArgs e)
-        {
-            Navigation.PushModalAsync(new ArgoPages.CompitiArgomenti("argomenti"));
-        }
+        //void Voti_Tapped(object sender, System.EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new ArgoPages.Voti());
+        //}
+
+        //void VotiScru_Tapped(object sender, System.EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new ArgoPages.VotiScrutinio());
+        //}
+
+        //void Compiti_Tapped(object sender, System.EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new ArgoPages.CompitiArgomenti("compiti"));
+        //}
+
+        //void Argomenti_Tapped(object sender, System.EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new ArgoPages.CompitiArgomenti("argomenti"));
+        //}
     }
 }
---
+++ SalveminiApp/SalveminiApp/Helpers/ArgoWidget.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace SalveminiApp
+{
+    public partial class ArgoWidget : ContentView
+    {
+        public static Page pushPage;
+
+        //Title
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(nameof(Title), typeof(string), typeof(WidgetGradient), default(string), Xamarin.Forms.BindingMode.OneWay);
+        public string Title
+        {
+            get
+            {
+                return (string)GetValue(TitleProperty);
+            }
+
+            set
+            {
+                SetValue(TitleProperty, value);
+            }
+        }
+
+        //Icon
+        public static readonly BindableProperty IconProperty = BindableProperty.Create(nameof(Icon), typeof(string), typeof(WidgetGradient), default(string), Xamarin.Forms.BindingMode.OneWay);
+        public string Icon
+        {
+            get
+            {
+                return (string)GetValue(IconProperty);
+            }
+
+            set
+            {
+                SetValue(IconProperty, value);
+            }
+        }
+
+        //StartColor
+        public static readonly BindableProperty StartColorProperty = BindableProperty.Create(nameof(StartColor), typeof(string), typeof(WidgetGradient), default(string), Xamarin.Forms.BindingMode.OneWay);
+        public string StartColor
+        {
+            get
+            {
+                return (string)GetValue(StartColorProperty);
+            }
+
+            set
+            {
+                SetValue(StartColorProperty, value);
+            }
+        }
+
+        //EndColor
+        public static readonly BindableProperty EndColorProperty = BindableProperty.Create(nameof(EndColor), typeof(string), typeof(WidgetGradient), default(string), Xamarin.Forms.BindingMode.OneWay);
+        public string EndColor
+        {
+            get
+            {
+                return (string)GetValue(EndColorProperty);
+            }
+
+            set
+            {
+                SetValue(EndColorProperty, value);
+            }
+        }
+
+        //PushTo
+        public static readonly BindableProperty PushProperty = BindableProperty.Create(nameof(Push), typeof(Page), typeof(WidgetGradient), default(Page), Xamarin.Forms.BindingMode.OneWay);
+        public Page Push
+        {
+            get
+            {
+                return (Page)GetValue(PushProperty);
+            }
+
+            set
+            {
+                SetValue(PushProperty, value);
+            }
+        }
+
+        //Order
+        public static readonly BindableProperty OrderProperty = BindableProperty.Create(nameof(Order), typeof(int), typeof(WidgetGradient), default(int), Xamarin.Forms.BindingMode.OneWay);
+        public int Order
+        {
+            get
+            {
+                return (int)GetValue(OrderProperty);
+            }
+
+            set
+            {
+                SetValue(OrderProperty, value);
+            }
+        }
+
+
+        public ArgoWidget()
+        {
+            InitializeComponent();
+
+            //Set dimensions
+            image.WidthRequest = App.ScreenWidth / 3.5;
+            image.HeightRequest = App.ScreenWidth / 3.5;
+            view.WidthRequest = App.ScreenWidth / 3;
+            view.HeightRequest = App.ScreenWidth / 3;
+            mainLayout.WidthRequest = App.ScreenWidth / 3;
+            mainLayout.HeightRequest = App.ScreenWidth / 3;
+
+        }
+
+        //Update values
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            try
+            {
+                //if (string.IsNullOrEmpty(Title) && string.IsNullOrEmpty(Icon) && string.IsNullOrEmpty(StartColor) && string.IsNullOrEmpty(EndColor) && Push == null)
+                //{
+                //    return;
+                //}
+
+
+                //Title
+                if (propertyName == TitleProperty.PropertyName)
+                {
+                    if (!string.IsNullOrEmpty(Title))
+                    TitleLbl.Text = Title;
+                }
+
+                //Icon
+                if (propertyName == IconProperty.PropertyName)
+                {
+                    if (!string.IsNullOrEmpty(Icon))
+                        image.Source = Icon;
+                }
+
+                //StartColor
+                if (propertyName == StartColorProperty.PropertyName)
+                {
+                    if (!string.IsNullOrEmpty(StartColor))
+                        view.BackgroundGradientStartColor = Color.FromHex(StartColor);
+                }
+
+                //EndColor
+                if (propertyName == EndColorProperty.PropertyName)
+                {
+                    if (!string.IsNullOrEmpty(EndColor))
+                        view.BackgroundGradientEndColor = Color.FromHex(EndColor);
+                }
+
+                //Push
+                if (propertyName == PushProperty.PropertyName)
+                {
+                    if (Push != null)
+                    pushPage = Push;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Boh per sicurezza a volte fa cose strane
+                Debug.WriteLine(ex);
+                return;
+            }
+
+        }
+    }
+}
