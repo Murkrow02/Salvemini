efff4246496609c07b102fd1ce33aa9c59db8394
Murkrow02 <38087157+Murkrow02@users.noreply.github.com>
2021-10-05 12:49:15 +0200

    Cleanup

--- SalveminiApp/SalveminiIntents/build/SalveminiIntents.build/Release-iphoneos/SalveminiIntents.build/Objects-normal/arm64/OrarioIntent.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/murkrow/Desktop/SalveminiIntents/SalveminiIntents/OrarioIntent.m \
-  /Users/murkrow/Desktop/SalveminiIntents/SalveminiIntents/OrarioIntent.h
--- SalveminiApp/SalveminiIntents/build/SalveminiIntents.build/Release-iphoneos/SalveminiIntents.build/Objects-normal/arm64/TrenoIntent.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/murkrow/Desktop/SalveminiIntents/SalveminiIntents/TrenoIntent.m \
-  /Users/murkrow/Desktop/SalveminiIntents/SalveminiIntents/TrenoIntent.h
